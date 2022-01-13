a326e54bc1d1aac8af1bea7286cf5239d10b6643
Alexander Guschin <1aguschin@gmail.com>
2022-01-13 16:10:53 +0300

    bugfix

--- gitops/__init__.py
+++ gitops/__init__.py
@@ -8,4 +8,4 @@ def init_registry(repo: git.Repo = git.Repo("."), base=CONFIG.BASE):
     if base != "tag":
         raise ValueError("Other implementations except of tag-based aren't supported")
     base_map = {"tag": TagsBasedRegistry}
-    return base_map[base]
+    return base_map[base]()
