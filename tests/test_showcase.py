b7dfaac6b69bae2b6c30ff3140c45dafcf2eef26
Alexander Guschin <1aguschin@gmail.com>
2022-02-22 11:22:17 +0300

    fix test with git.Repo(.).commit().author.name

--- tests/test_showcase.py
+++ tests/test_showcase.py
@@ -1,8 +1,8 @@
 """TODO: break this file into multiple test/files"""
-import getpass
 import os.path
 from typing import Any, Dict, Set
 
+import git
 import pytest
 from pydantic import BaseModel
 
@@ -19,8 +19,8 @@ def init_showcase(empty_git_repo):
     def write_file(name, content):
         fullpath = os.path.join(path, name)
         os.makedirs(os.path.dirname(fullpath), exist_ok=True)
-        with open(fullpath, "w") as f:
-            f.write(content)
+        with open(fullpath, "w", encoding="utf8") as file:
+            file.write(content)
 
     write_file(CONFIG_FILE, "env_base: tag")
 
@@ -32,8 +32,8 @@ def _check_dict(obj: BaseModel, values: Dict[str, Any], skip_keys: Set[str]):
     assert obj_values == values
 
 
-def test_api(init_showcase):
-    path, repo, write_file = init_showcase
+def test_api(init_showcase):  # pylint: disable=too-many-locals
+    path, repo, write_file = init_showcase  # pylint: disable=unused-variable
 
     write_file("models/random-forest.pkl", "1st version")
     write_file("models/neural-network.pkl", "1st version")
@@ -69,25 +69,39 @@ def test_api(init_showcase):
 
     objects = registry.state.objects
     assert set(objects.keys()) == {"features", "nn", "rf"}
-    assert objects["features"] == BaseObject(name='features', versions=[], labels=[])
+    assert objects["features"] == BaseObject(name="features", versions=[], labels=[])
     nn_object = objects["nn"]
     assert isinstance(nn_object, BaseObject)
     assert nn_object.name == "nn"
     assert len(nn_object.versions) == 1
     nn_version = nn_object.versions[0]
     assert isinstance(nn_version, BaseVersion)
-    _check_dict(nn_version, dict(object='nn',
-                                 name='v1',
-                                 author=getpass.getuser(),
-                                 commit_hexsha=first_commit.hexsha,
-                                 unregistered_date=None), {"creation_date"})
+    _check_dict(
+        nn_version,
+        dict(
+            object="nn",
+            name="v1",
+            author=git.Repo(".").commit().author.name,
+            commit_hexsha=first_commit.hexsha,
+            unregistered_date=None,
+        ),
+        {"creation_date"},
+    )
     assert len(nn_object.labels) == 1
     nn_label = nn_object.labels[0]
     assert isinstance(nn_label, BaseLabel)
-    _check_dict(nn_label, dict(object='nn', version='v1', name='staging',
-                               author=getpass.getuser(),
-                               commit_hexsha=first_commit.hexsha,
-                               unregistered_date=None), {"creation_date"})
+    _check_dict(
+        nn_label,
+        dict(
+            object="nn",
+            version="v1",
+            name="staging",
+            author=git.Repo(".").commit().author.name,
+            commit_hexsha=first_commit.hexsha,
+            unregistered_date=None,
+        ),
+        {"creation_date"},
+    )
 
     rf_object = objects["rf"]
     assert isinstance(rf_object, BaseObject)
@@ -96,29 +110,70 @@ def test_api(init_showcase):
     assert len(rf_object.versions) == 2
     assert all(isinstance(v, BaseVersion) for v in rf_object.versions)
     rf_ver1, rf_ver2 = rf_object.versions
-    _check_dict(rf_ver1, dict(object='rf', name='v1',
-                              author=getpass.getuser(), commit_hexsha=first_commit.hexsha,
-                              unregistered_date=None), {"creation_date"})
-    _check_dict(rf_ver2, dict(object='rf', name='v2',
-                              author=getpass.getuser(), commit_hexsha=second_commit.hexsha,
-                              unregistered_date=None), {"creation_date"})
+    author = git.Repo(".").commit().author.name
+    _check_dict(
+        rf_ver1,
+        dict(
+            object="rf",
+            name="v1",
+            author=author,
+            commit_hexsha=first_commit.hexsha,
+            unregistered_date=None,
+        ),
+        {"creation_date"},
+    )
+    _check_dict(
+        rf_ver2,
+        dict(
+            object="rf",
+            name="v2",
+            author=author,
+            commit_hexsha=second_commit.hexsha,
+            unregistered_date=None,
+        ),
+        {"creation_date"},
+    )
 
     assert len(rf_object.labels) == 3
     assert all(isinstance(l, BaseLabel) for l in rf_object.labels)
     rf_l1, rf_l2, rf_l3 = rf_object.labels
 
-    _check_dict(rf_l1, dict(object='rf', version='v1', name='production',
-                            author=getpass.getuser(),
-                            commit_hexsha=first_commit.hexsha, unregistered_date=None),
-                {"creation_date"})
-    _check_dict(rf_l2, dict(object='rf', version='v2', name='production',
-                            author=getpass.getuser(),
-                            commit_hexsha=second_commit.hexsha, unregistered_date=None),
-                {"creation_date"})
-    _check_dict(rf_l3, dict(object='rf', version='v2', name='staging',
-                            author=getpass.getuser(),
-                            commit_hexsha=second_commit.hexsha, unregistered_date=None),
-                {"creation_date"})
+    _check_dict(
+        rf_l1,
+        dict(
+            object="rf",
+            version="v1",
+            name="production",
+            author=author,
+            commit_hexsha=first_commit.hexsha,
+            unregistered_date=None,
+        ),
+        {"creation_date"},
+    )
+    _check_dict(
+        rf_l2,
+        dict(
+            object="rf",
+            version="v2",
+            name="production",
+            author=author,
+            commit_hexsha=second_commit.hexsha,
+            unregistered_date=None,
+        ),
+        {"creation_date"},
+    )
+    _check_dict(
+        rf_l3,
+        dict(
+            object="rf",
+            version="v2",
+            name="staging",
+            author=author,
+            commit_hexsha=second_commit.hexsha,
+            unregistered_date=None,
+        ),
+        {"creation_date"},
+    )
 
 
 def test_cli(init_showcase):
