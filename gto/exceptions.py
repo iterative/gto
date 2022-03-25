bb069091765162a4a2b8d52b5e7dc5f22af2419b
Alexander Guschin <1aguschin@gmail.com>
2022-03-25 19:11:50 +0600

    remove extra lines in exception

--- gto/exceptions.py
+++ gto/exceptions.py
@@ -10,9 +10,6 @@ class GTOException(Exception):
 class NotFound(GTOException):
     """Raised when a requested resource is not found."""
 
-    def __init__(self, msg, *args):  # pylint: disable=useless-super-delegation
-        super().__init__(msg, *args)
-
 
 class NoRepo(GTOException):
     _message = "No Git repo found in '{path}'"
