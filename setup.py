540ce4a61ca8a097df4758f087344bde5bb57a4d
Alexander Guschin <1aguschin@gmail.com>
2022-04-16 21:50:28 +0300

    gto: bump to 0.1.2

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ tests = [
 
 setup_args = dict(  # noqa: C408
     name="gto",
-    version="0.1.1",
+    version="0.1.2",
     description="Version and deploy your models following GitOps principles",
     long_description=(Path(__file__).parent / "README.md").read_text(encoding="utf8"),
     long_description_content_type="text/markdown",
