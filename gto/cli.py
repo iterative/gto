c2985fd0914c4ac595846c2ee7c704ccb6443854
Francesco Calcavecchia <francesco086@users.noreply.github.com>
2022-11-01 10:36:49 +0100

    cli help mention experimental support for remote repos (#297)
    * update cli help
    
    * 2nd update cli help
    
    Co-authored-by: Francesco Calcavecchia <f18771@eon.com>

--- gto/cli.py
+++ gto/cli.py
@@ -218,7 +218,7 @@ option_repo = Option(
     ".",
     "-r",
     "--repo",
-    help="Repository to use",
+    help="Local or remote repository",
     show_default=True,
 )
 option_all_branches = Option(
