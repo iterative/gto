0dfde30e0fb0976ea6def059775dd7b43e467f94
Alexander Guschin <1aguschin@gmail.com>
2023-05-05 11:35:17 +0300

    allow numbers as first char of artifact name

--- gto/constants.py
+++ gto/constants.py
@@ -42,7 +42,7 @@ def tag_to_name(value):
 
 
 dirname = "[a-z0-9-_./]+"  # improve?
-name = r"[a-z]([a-z0-9-/]*[a-z0-9])?"  # just like in DVC now + "/"
+name = r"[a-z0-9]([a-z0-9-/]*[a-z0-9])?"
 semver = r"(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?"
 counter = "?P<counter>[0-9]+"
 name_re = re.compile(f"^{name}$")
