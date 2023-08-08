4ef0b9b6097027bd97266d958409581b2998a5bb
Peter Rowlands <peter@pmrowla.com>
2023-08-08 20:40:33 +0900

    conftest: migrate to pytest-test-utils/scmrepo

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,12 @@
-import os
+# pylint: disable=redefined-outer-name
 import sys
 from time import sleep
-from typing import Callable, Tuple
+from typing import Tuple
 
-import git
 import pytest
 from click.testing import Result
+from pytest_test_utils import TmpDir
+from scmrepo.git import Git
 from typer.testing import CliRunner
 
 import gto
@@ -26,48 +27,40 @@ def runner() -> Runner:
     return Runner()
 
 
+@pytest.fixture
+def scm(tmp_dir: TmpDir) -> Git:
+    scm_instance = Git.init(tmp_dir)
+    try:
+        yield scm_instance
+    finally:
+        scm_instance.close()
+
+
 @pytest.fixture(autouse=True)
 def set_recursion_limit() -> None:
     sys.setrecursionlimit(2000)
 
 
 @pytest.fixture
-def empty_git_repo(tmp_path):
-    repo = git.Repo.init(tmp_path)
-
-    def write_file(name, content):
-        fullpath = os.path.join(tmp_path, name)
-        os.makedirs(os.path.dirname(fullpath), exist_ok=True)
-        with open(fullpath, "w", encoding="utf8") as file:
-            file.write(content)
-
-    return repo, write_file
+def empty_git_repo(scm: Git) -> str:
+    return scm.root_dir
 
 
 @pytest.fixture
-def repo_with_commit(empty_git_repo: Tuple[git.Repo, Callable]):
-    repo, write_file = empty_git_repo
-
-    write_file(
-        "some-random-file",
+def repo_with_commit(tmp_dir: TmpDir, scm: Git) -> str:
+    tmp_dir.gen(
+        "some-tracked-file",
         "some-random-text",
     )
-    repo.index.add(["some-random-file"])
-    repo.index.commit("Initial commit")
-
-    return repo, write_file
+    scm.add("some-tracked-file")
+    scm.commit("Initial commit")
+    return str(tmp_dir)
 
 
 @pytest.fixture
-def init_showcase_semver(empty_git_repo: Tuple[git.Repo, Callable]):
-    repo, write_file = empty_git_repo
-
-    write_file(
-        CONFIG_FILE_NAME,
-        "",
-    )
-
-    return repo, write_file
+def init_showcase_semver(tmp_dir: TmpDir, request: pytest.FixtureRequest):
+    request.getfixturevalue("scm")
+    tmp_dir.gen(CONFIG_FILE_NAME, "")
 
 
 artifacts_yaml = """
@@ -87,43 +80,44 @@ features:
 
 @pytest.fixture
 def showcase(
-    init_showcase_semver,
-):  # pylint: disable=too-many-locals, too-many-statements
-    repo: git.Repo
-    repo, write_file = init_showcase_semver
-    path = repo.working_dir
+    tmp_dir: TmpDir, scm: Git, request: pytest.FixtureRequest
+) -> Tuple[str, str]:  # pylint: disable=too-many-locals, too-many-statements
+    request.getfixturevalue("init_showcase_semver")
+    tmp_dir.gen(
+        {
+            "models/random-forest.pkl": "1st version",
+            "models/neural-network.pkl": "1st version",
+        }
+    )
+    scm.add(["models"])
+    scm.commit("Create models")
 
-    write_file("models/random-forest.pkl", "1st version")
-    write_file("models/neural-network.pkl", "1st version")
-    repo.index.add(["models"])
-    repo.index.commit("Create models")
-
-    with open(os.path.join(path, "artifacts.yaml"), "w", encoding="utf8") as file:
-        file.write(artifacts_yaml)
-
-    repo.index.add(["artifacts.yaml"])
-    first_commit = repo.index.commit("Add artifacts")
+    tmp_dir.gen("artifacts.yaml", artifacts_yaml)
+    scm.add(["artifacts.yaml"])
+    scm.commit("Add artifacts")
+    first_commit = scm.get_rev()
 
     nn_vname = "v0.0.1"
     rf_vname = "v1.2.3"
-    gto.api.register(path, "rf", "HEAD", rf_vname)
-    gto.api.register(path, "nn", "HEAD")
+    gto.api.register(tmp_dir, "rf", "HEAD", rf_vname)
+    gto.api.register(tmp_dir, "nn", "HEAD")
     sleep(1)
 
-    write_file("models/random-forest.pkl", "2nd version")
+    tmp_dir.gen("models/random-forest.pkl", "2nd version")
 
-    second_commit = repo.index.commit("Update model")
+    scm.commit("Update model")
+    second_commit = scm.get_rev()
 
     # bump version automatically
-    gto.api.register(path, "rf", "HEAD")
+    gto.api.register(tmp_dir, "rf", "HEAD")
 
-    gto.api.assign(path, "nn", "staging", version=nn_vname)
-    gto.api.assign(path, "rf", "production", version=rf_vname)
+    gto.api.assign(tmp_dir, "nn", "staging", version=nn_vname)
+    gto.api.assign(tmp_dir, "rf", "production", version=rf_vname)
     sleep(1)  # this is needed to ensure right order of assignments in later checks
     # the problem is git tags doesn't have miliseconds precision, so we need to wait a bit
-    gto.api.assign(path, "rf", "staging", ref="HEAD")
+    gto.api.assign(tmp_dir, "rf", "staging", ref="HEAD")
     sleep(1)
-    gto.api.assign(path, "rf", "production", ref=repo.head.ref.commit.hexsha)
+    gto.api.assign(tmp_dir, "rf", "production", ref=scm.get_rev())
     sleep(1)
-    gto.api.assign(path, "rf", "production", version=rf_vname, force=True)
-    return path, repo, write_file, first_commit, second_commit
+    gto.api.assign(tmp_dir, "rf", "production", version=rf_vname, force=True)
+    return first_commit, second_commit
