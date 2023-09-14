9e778f88a413e6c3b1cff78092d4de65d8941ccc
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2023-09-14 18:22:32 +0545

    setup.py: flake8-comprehension: fix issue

--- setup.py
+++ setup.py
@@ -32,22 +32,24 @@ tests = [
 ]
 
 
-setup_args = dict(  # noqa: C408
-    name="gto",
-    use_scm_version=True,
-    setup_requires=["setuptools_scm", "fastentrypoints>=0.12"],
-    description="Version and deploy your models following GitOps principles",
-    long_description=(Path(__file__).parent / "README.md").read_text(encoding="utf8"),
-    long_description_content_type="text/markdown",
-    author="Alexander Guschin",
-    author_email="aguschin@iterative.ai",
-    download_url="https://github.com/iterative/gto",
-    license="Apache License 2.0",
-    install_requires=install_requires,
-    extras_require={"tests": tests},
-    keywords="git repo repository artifact registry developer-tools collaboration",
-    python_requires=">=3.8",
-    classifiers=[
+setup_args = {
+    "name": "gto",
+    "use_scm_version": True,
+    "setup_requires": ["setuptools_scm", "fastentrypoints>=0.12"],
+    "description": "Version and deploy your models following GitOps principles",
+    "long_description": (Path(__file__).parent / "README.md").read_text(
+        encoding="utf8"
+    ),
+    "long_description_content_type": "text/markdown",
+    "author": "Alexander Guschin",
+    "author_email": "aguschin@iterative.ai",
+    "download_url": "https://github.com/iterative/gto",
+    "license": "Apache License 2.0",
+    "install_requires": install_requires,
+    "extras_require": {"tests": tests},
+    "keywords": "git repo repository artifact registry developer-tools collaboration",
+    "python_requires": ">=3.8",
+    "classifiers": [
         "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
@@ -55,16 +57,16 @@ setup_args = dict(  # noqa: C408
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
-    packages=find_packages(exclude=["tests"]),
-    include_package_data=True,
-    entry_points={
+    "packages": find_packages(exclude=["tests"]),
+    "include_package_data": True,
+    "entry_points": {
         "console_scripts": ["gto = gto.cli:app"],
         "gto.enrichment": [
             "gto = gto.index:GTOEnrichment",
         ],
     },
-    zip_safe=False,
-)
+    "zip_safe": False,
+}
 
 if __name__ == "__main__":
     setup(**setup_args)
