f8e6cec0013ed9104a4d973e32ed048dfb377256
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2023-09-15 11:46:42 +0545

    remove pydantic compat

--- tests/utils.py
+++ tests/utils.py
@@ -3,8 +3,7 @@ from copy import deepcopy
 from typing import Any, Dict, Sequence, Set, Union
 
 from funcy import omit
-
-from gto._pydantic import BaseModel
+from pydantic import BaseModel
 
 
 def show_difference(left: Dict, right: Dict):
